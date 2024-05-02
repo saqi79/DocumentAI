0c8a7c20bc623f4e83f1adf5032641828f4aa0b5
aarifansari99 <aarif454b@gmail.com>
2024-05-02 17:49:45 +0530

    final push

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,98 +10,119 @@ import Fire from "../../Images/ball.svg";
 const Footer = () => {
   return (
     <>
-      <div className="footer-container container pt-5">
+      <div className="footer-container container pt-5 pb-5">
         <footer>
-          <Row>
-            <Col md={6}>
-              <div className="co-discrip">
-                <div className="co-head">
-                  <h2>Document AL</h2>
-                  <p>
-                    Transforming Ideas into Digital Excellence. Elevate your
-                    online presence with our innovative solutions and strategic
-                    digital services.
-                  </p>
+          <div className="footer-content pt-5 pb-5">
+            <Row>
+              <Col md={6}>
+                <div className="co-discrip">
+                  <div className="co-head">
+                    <h2>Document AI</h2>
+                    <p>
+                      Transforming Ideas into Digital Excellence. Elevate your
+                      online presence with our innovative solutions and
+                      strategic digital services.
+                    </p>
+                  </div>
+                  <div className="socials">
+                    <img src={Facebook} alt="" />
+                    <img src={Instagram} alt="" />
+                    <img src={youtube} alt="" />
+                    <img src={Linkedin} alt="" />
+                    <img src={Fire} alt="" />
+                  </div>
                 </div>
-                <div className="socials">
-                  <img src={Facebook} alt="" />
-                  <img src={Instagram} alt="" />
-                  <img src={youtube} alt="" />
-                  <img src={Linkedin} alt="" />
-                  <img src={Fire} alt="" />
+              </Col>
+              <Col md={2}>
+                <div className="Services">
+                  <h2 className="head-clr">Services</h2>
+                  <div className="all-Service font-style">
+                    <a href="">
+                      <span>Website Development</span>
+                    </a>
+                    <a href="">
+                      <span>App Development</span>
+                    </a>
+                    <a href="">
+                      <span>Digital Marketing</span>
+                    </a>
+                    <a href="">
+                      <span>Graphic Design</span>
+                    </a>
+                    <a href="">
+                      <span>Brand Identity</span>
+                    </a>
+                    <a href="">
+                      <span>Search Engine Optimization</span>
+                    </a>
+                  </div>
                 </div>
-              </div>
-            </Col>
-            <Col md={2}>
-              <div className="Services">
-                <h2 className="head-clr">Services</h2>
-                <div className="all-Service font-style">
-                  <a href="">
-                    <span>Website Development</span>
-                  </a>
-                  <a href="">
-                    <span>App Development</span>
-                  </a>
-                  <a href="">
-                    <span>Digital Marketing</span>
-                  </a>
-                  <a href="">
-                    <span>Graphic Design</span>
-                  </a>
-                  <a href="">
-                    <span>Brand Identity</span>
-                  </a>
-                  <a href="">
-                    <span>Search Engine Optimization</span>
-                  </a>
+              </Col>
+              <Col md={2}>
+                <div className="Support">
+                  <h2 className="head-clr">Support</h2>
+                  <div className="all-Support font-style">
+                    <a href="">
+                      <span>Help Center</span>
+                    </a>
+                    <a href="">
+                      <span>FAQ</span>
+                    </a>
+                    <a href="">
+                      <span>Ticket Support</span>
+                    </a>
+                    <a href="">
+                      <span>Sales Support</span>
+                    </a>
+                    <a href="">
+                      <span>Contact us</span>
+                    </a>
+                  </div>
                 </div>
-              </div>
-            </Col>
-            <Col md={2}>
-              <div className="Support">
-                <h2 className="head-clr">Support</h2>
-                <div className="all-Support font-style">
-                  <a href="">
-                    <span>Help Center</span>
-                  </a>
-                  <a href="">
-                    <span>FAQ</span>
-                  </a>
-                  <a href="">
-                    <span>Ticket Support</span>
-                  </a>
-                  <a href="">
-                    <span>Sales Support</span>
-                  </a>
-                  <a href="">
-                    <span>Contact us</span>
-                  </a>
+              </Col>
+              <Col md={2}>
+                <div className="Company">
+                  <h2 className="head-clr">Company</h2>
+                  <div className="about-co font-style">
+                    <a href="">
+                      <span>About us</span>
+                    </a>
+                    <a href="">
+                      <span>Our Team</span>
+                    </a>
+                    <a href="">
+                      <span>Careers</span>
+                    </a>
+                    <a href="">
+                      <span>Article & News</span>
+                    </a>
+                    <a href="">
+                      <span>Legal Notices</span>
+                    </a>
+                  </div>
                 </div>
-              </div>
-            </Col>
-            <Col md={2}>
-            <div className="Company">
-                <h2 className="head-clr">Company</h2>
-                <div className="about-co font-style">
-                  <a href="">
-                    <span>About us</span>
-                  </a>
-                  <a href="">
-                    <span>Our Team</span>
-                  </a>
-                  <a href="">
-                    <span>Careers</span>
-                  </a>
-                  <a href="">
-                    <span>Article & News</span>
-                  </a>
-                  <a href="">
-                    <span>Legal Notices</span>
-                  </a>
-                </div>
-              </div>
-            </Col>
-          </Row>
+              </Col>
+            </Row>
+          </div>
+          <div className="copy-rights-content pt-5">
+            <div className="left-part">
+              <h3>
+                Copyright© 2023 DigiMax, All rights reserved. Powered by
+                MoxCreative.
+              </h3>
+            </div>
+            <div className="right-part">
+              <a href="">
+                <span>Term of use</span>
+              </a>
+              <a href="">
+                <span>Privacy Policy</span>
+              </a>
+              <a href="">
+                <span>Cookie Policy</span>
+              </a>
+            </div>
+          </div>
         </footer>
       </div>
     </>
